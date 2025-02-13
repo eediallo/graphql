c3dbff8160ef92d4638fd0b1cb53d0f151d85420
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-13 18:29:33 +0000

    export form element from queries.js

--- queries.js
+++ queries.js
@@ -1,5 +1,6 @@
 const usernameEl = document.querySelector("#username");
 const passwordEl = document.querySelector("#password");
 const loginBtn = document.querySelector("#loginBtn");
+const formEl = document.querySelector("form");
 
-export { usernameEl, passwordEl, loginBtn };
+export { usernameEl, passwordEl, loginBtn, formEl };
