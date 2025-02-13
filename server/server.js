15b03cafff86c370613a5d5a665a0e00501cfa61
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-13 16:51:20 +0000

    add GraphQL schema definition to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
+
+schema {
+    query: Query
+}
 type Query {
  greeting: String
 }
@@ -13,6 +17,6 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
 console.log(`Server is running at port ${url}`);
