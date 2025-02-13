9eb23fadc7eadfe8ee5acc3cbef5032f89d69a62
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-13 18:29:25 +0000

    implement login functionality with API integration in script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello!!!!!!!!!!!!!");
+import { usernameEl, passwordEl, loginBtn } from "./queries.js";
+const loginEndpoint = "https://learn.01founders.co/api/auth/signin";
+const credentials = {
+  username: usernameEl.value.trim(),
+  password: passwordEl.value.trim(),
+};
+
+async function fetchLoginEndPoints() {
+  console.log(credentials, "credentials");
+  const resp = await fetch(loginEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  const data = await resp.json();
+  return data;
+}
+
+async function loginHandler() {
+  const credentials = await fetchLoginEndPoints();
+  console.log(credentials);
+}
+
+loginBtn.addEventListener("click", loginHandler);
