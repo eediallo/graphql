2916ee8dde2f7767e7741194e5bc039d4fe9e0c7
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 20:26:16 +0000

    fix: update logout handler import in attachHandlers.js

--- eventHandlers/attachHandlers.js
+++ eventHandlers/attachHandlers.js
@@ -1,10 +1,10 @@
 import { loginBtn, logoutBtn } from "../queries/domQueries.js";
 import { loginAndRetriveUserData } from "../data/retriveUserData.js";
-import { logoutHandler } from "./logout.js";
+import { logout } from "./logout.js";
 
 export function attachEventHandlers() {
   loginBtn.addEventListener("click", async (e) => {
     await loginAndRetriveUserData(e);
   });
-  logoutBtn.addEventListener("click", logoutHandler);
+  logoutBtn.addEventListener("click", logout);
 }
