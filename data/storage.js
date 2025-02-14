e8b1edb41bc07a9e10d4e7b83bebf129d71e0ee9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 20:55:53 +0000

    feat: add setUserData function to store user data in local storage

--- data/storage.js
+++ data/storage.js
@@ -6,6 +6,10 @@ export function saveJwtToLocalStorage(jwtToken) {
   localStorage.setItem("jwt", jwtToken);
 }
 
+export function setUserData(userId, data) {
+  localStorage.setItem(userId, JSON.stringify(data));
+}
+
 export function removeJwtFromLocalStorage() {
   localStorage.removeItem("jwt");
 }
