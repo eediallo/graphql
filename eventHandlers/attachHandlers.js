db60001b05024d9a6ad740078a1e661e8a75a212
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 19:05:27 +0000

    feat: enhance login button handler to fetch and log user data after login

--- eventHandlers/attachHandlers.js
+++ eventHandlers/attachHandlers.js
@@ -1,7 +1,14 @@
-import { loginBtn, logoutBtn } from "../queries.js";
+import { loginBtn, logoutBtn } from "../queries/domQueries.js";
 import { loginHandler, logoutHandler } from "./eventHandlers.js";
+import { getUserData } from "../data/getUserData.js";
+import { retrieveJwtFromStorage } from "../data/storage.js";
 
 export function attachEventHandlers() {
-  loginBtn.addEventListener("click", loginHandler);
+  loginBtn.addEventListener("click", async () => {
+    loginHandler();
+    const jwt = retrieveJwtFromStorage();
+    const userTable = await getUserData(jwt);
+    console.log(userTable);
+  });
   logoutBtn.addEventListener("click", logoutHandler);
 }
