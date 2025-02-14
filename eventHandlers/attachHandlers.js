665761124c2b2d2aed91cbcb52c546c1d0f1f32d
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 19:40:36 +0000

    feat: update login button handler to use loginAndRetriveUserData for user login and data retrieval

--- eventHandlers/attachHandlers.js
+++ eventHandlers/attachHandlers.js
@@ -1,14 +1,10 @@
 import { loginBtn, logoutBtn } from "../queries/domQueries.js";
-import { loginHandler, logoutHandler } from "./eventHandlers.js";
-import { getUserData } from "../data/getUserData.js";
-import { retrieveJwtFromStorage } from "../data/storage.js";
+import { loginAndRetriveUserData } from "../data/retriveUserData.js";
+import { logoutHandler } from "./eventHandlers.js";
 
 export function attachEventHandlers() {
-  loginBtn.addEventListener("click", async () => {
-    loginHandler();
-    const jwt = retrieveJwtFromStorage();
-    const userTable = await getUserData(jwt);
-    console.log(userTable);
+  loginBtn.addEventListener("click", async (e) => {
+    await loginAndRetriveUserData(e);
   });
   logoutBtn.addEventListener("click", logoutHandler);
 }
