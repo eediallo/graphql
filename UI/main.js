52dadaa80f17f3ad3533886183824e00fbe67168
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 21:59:43 +0000

    feat: integrate displayFirstName function to show user's first name on data retrieval

--- UI/main.js
+++ UI/main.js
@@ -1,9 +1,13 @@
 import { getUserId } from "../data/getUserId.js";
 import { getData } from "../data/storage.js";
+import { displayFirstName } from "./displayFirstName.js";
 
 function main() {
   const userId = getUserId();
   const userData = getData(userId);
+  for (const { attrs, campus } of userData.data.user) {
+    displayFirstName(attrs.firstName);
+  }
 }
 
 window.onload = main;
