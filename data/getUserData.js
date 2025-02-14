4740dd58f6acf3b827afe9864aaf8b615ad0485e
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-14 19:05:16 +0000

    feat: add getUserData function to fetch user data from GraphQL API

---
+++ data/getUserData.js
@@ -0,0 +1,37 @@
+const profileQuery = `#graphql
+{
+    user {
+        attrs
+        campus
+    }
+}`;
+
+const dataEndpoint =
+  "https://learn.01founders.co/api/graphql-engine/v1/graphql";
+
+export async function getUserData(jwt) {
+  try {
+    const resp = await fetch(dataEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`, // Include the JWT token
+      },
+      body: JSON.stringify({ query: profileQuery }),
+    });
+
+    if (!resp.ok) {
+      const errorText = await resp.text(); // Capture response text in case of error
+      throw new Error(
+        `HTTP error! Status: ${resp.status}. Response: ${errorText}`
+      );
+    }
+
+    const data = await resp.json();
+    console.log(data, "user table");
+    return data;
+  } catch (error) {
+    console.error("Error fetching user table:", error);
+    throw error;
+  }
+}
