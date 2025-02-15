a12b74979999a9f8cfbcf0db3f13a0f88bed3552
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-15 08:14:06 +0000

    feat: update retriveUserData to include dynamic GraphQL query for user data retrieval

--- data/retriveUserData.js
+++ data/retriveUserData.js
@@ -1,8 +1,9 @@
 import { getData } from "./storage.js";
 import { getUserData } from "./getUserData.js";
+import { graphqlQueries } from "../queries/grapqlQueries.js";
 
 export async function retriveUserData() {
   const jwt = getData("jwt");
-  const userData = await getUserData(jwt);
+  const userData = await getUserData(jwt, graphqlQueries.profileQuery);
   return userData;
 }
