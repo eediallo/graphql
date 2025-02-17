357fd7d914ad8178a06f868f3e34dd9b456b0537
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-17 14:55:09 +0000

    feat: add campus and path headers to the transaction table

--- UI/createTable.js
+++ UI/createTable.js
@@ -41,9 +41,23 @@ export function createTableCeils(data) {
   return transTr;
 }
 
+function createTableHeadCeils(data) {
+  const transId = createElement(data.id, "th");
+  const transType = createElement(data.type, "th");
+  const transAmount = createElement(data.amount, "th");
+  const transCampus = createElement(data.campus, "th");
+  const transPath = createElement(data.path, "th");
+  const transTr = createAndAppendToElement(
+    [transId, transType, transAmount, transCampus, transPath],
+    "tr"
+  );
+
+  return transTr;
+}
+
 function createTableHeader() {
-  const tableCeils = tableHeaders.map(createTableCeils);
-  const tHeader = createAndAppendToElement(tableCeils, "th");
+  const tableCeils = tableHeaders.map(createTableHeadCeils);
+  const tHeader = createAndAppendToElement(tableCeils, "thead");
   return tHeader;
 }
 
