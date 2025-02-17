717d10fa198cfa4325b6ffc1d2a24917f0258bd8
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-17 11:50:28 +0000

    feat: add rewardTransactionQuery and progressQuery to graphqlQueries

--- queries/grapqlQueries.js
+++ queries/grapqlQueries.js
@@ -6,15 +6,56 @@ const profileQuery = `#graphql
     }
 }`;
 
-const transactionQuery = `#graphql
+const rewardTransactionQuery = `#graphql
 {
-  user {
-    xp
+  transaction {
+    id,
+    type,
+    amount,
+    userId,
+    attrs,
+    createdAt,
+    path,
+    objectId,
+    eventId,
+    campus
   }
 }
+`;
 
-`
+const roleQuery = `#graphql
+{
+  role {
+    id,
+    slug,
+    name,
+    description,
+    createdAt,
+    updatedAt
+  }
+}
+`;
+
+const progressQuery = `#graphql
+{
+  progress {
+    id,
+    createAt,
+    updatedAt,
+    userId,
+    groupId,
+    eventId,
+    version,
+    isDone,
+    path,
+    campus
+  }
+}
+`;
 
 export const graphqlQueries = {
   profileQuery,
+  roleQuery,
+  progressQuery,
+  rewardTransactionQuery,
 };
