7191de48ae43245672c53b0e41de99b68d85a7d0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-17 16:13:04 +0000

    refactor: update main.js to use displayFirstName from userProfile.js

--- UI/main.js
+++ UI/main.js
@@ -1,16 +1,9 @@
-import { getUserId } from "../data/getUserId.js";
-import { getData } from "../data/storage.js";
 import { renderTransTables } from "./transactionTables.js";
-import { displayFirstName } from "./displayFirstName.js";
+import { displayFirstName } from "./userProfile.js";
 
 function main() {
+  displayFirstName();
   renderTransTables();
-  const userId = getUserId();
-  const userData = getData(userId);
-  console.log(userData, "<====user DATA====");
-  for (const { attrs, campus } of userData.data.user) {
-    displayFirstName(attrs.firstName);
-  }
 }
 
 window.onload = main;
