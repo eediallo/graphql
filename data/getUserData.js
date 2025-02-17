87a650fd05067f7a755d17efe02234786ca505ef
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-17 10:49:01 +0000

    feat: rename getUserData function to getUserProfile for clarity

--- data/getUserData.js
+++ data/getUserData.js
@@ -1,7 +1,7 @@
 const dataEndpoint =
   "https://learn.01founders.co/api/graphql-engine/v1/graphql";
 
-export async function getUserData(jwt, query) {
+export async function getUserProfile(jwt, query) {
   try {
     const resp = await fetch(dataEndpoint, {
       method: "POST",
