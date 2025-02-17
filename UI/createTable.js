8641133943ab491f8aa71bcc6a519376fe5ebf37
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-17 14:40:18 +0000

    feat: enhance createTable.js to include campus and path in table headers and rows

--- UI/createTable.js
+++ UI/createTable.js
@@ -1,6 +1,15 @@
 import { getUserId } from "../data/getUserId.js";
 import { getData } from "../data/storage.js";
 
+const tableHeaders = [
+  {
+    id: "ID",
+    type: "Type",
+    amount: "Amount",
+    campus: "Campus",
+    path: "Path",
+  },
+];
 
 const userId = getUserId();
 const userData = getData(userId);
@@ -23,16 +32,25 @@ export function createTableCeils(data) {
   const transId = createElement(data.id, "td");
   const transType = createElement(data.type, "td");
   const transAmount = createElement(data.amount, "td");
-  const transCreatedAt = createElement(data.createdAt, "td");
+  const transCampus = createElement(data.campus, "td");
   const transPath = createElement(data.path, "td");
   const transTr = createAndToElement(
-    [transId, transType, transAmount, transCreatedAt, transPath],
+    [transId, transType, transAmount, transCampus, transPath],
     "tr"
   );
   return transTr;
 }
 
+function createTableHeader() {
+  const tableCeils = tableHeaders.map(createTableCeils);
+  console.log(tableCeils, "<===ceils");
+  const tHeader = createAndToElement(tableCeils, "th");
+  return tHeader;
+}
+
 export function createTableRow() {
+  const tHeader = createTableHeader();
+  console.log(tHeader);
   const tdansactionList = transactionData.map(createTableCeils);
-  document.body.append(...tdansactionList);
+  document.body.append(tHeader, ...tdansactionList);
 }
