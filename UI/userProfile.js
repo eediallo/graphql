6bc10058d4450fc7fdc327ffd1ab7d855c371d01
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-19 17:29:32 +0000

    feat: update heading levels in userProfile.js and dashboard.html for improved accessibility

--- UI/userProfile.js
+++ UI/userProfile.js
@@ -12,7 +12,7 @@ export function displayFirstName() {
 }
 
 export function createUserProfile() {
-  const fullName = createElement(`${firstName} ${lastName}`, "h1");
+  const fullName = createElement(`${firstName} ${lastName}`, "h2");
   const telEl = createElement(`<strong>Tel</strong>: ${tel}`, "p");
   const emailEl = createElement(`<strong>Email</strong>: ${email}`, "p");
   const locationEl = createElement(
