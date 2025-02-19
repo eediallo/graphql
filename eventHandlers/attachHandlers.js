42db34be9ba4774bd8eb4d76fca8c0f8e9505b21
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-19 11:05:54 +0000

    feat: add event handler for transaction button and improve button checks

--- eventHandlers/attachHandlers.js
+++ eventHandlers/attachHandlers.js
@@ -1,10 +1,17 @@
-import { loginBtn, logoutBtn } from "../queries/domQueries.js";
+import { goTransBtn, loginBtn, logoutBtn } from "../queries/domQueries.js";
 import { logout } from "./logout.js";
 import { login } from "./login.js";
+import { golangTransactionTable } from "../UI/transactionTables.js";
 
 export function attachEventHandlers() {
-  loginBtn.addEventListener("click", async (e) => {
-    await login(e);
-  });
-  logoutBtn.addEventListener("click", logout);
+  if (loginBtn) {
+    loginBtn.addEventListener("click", async (e) => {
+      await login(e);
+    });
+  }
+
+  if (loginBtn) {
+    logoutBtn.addEventListener("click", logout);
+  }
+  goTransBtn.addEventListener("click", golangTransactionTable);
 }
