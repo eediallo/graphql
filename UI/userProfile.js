9bea52507c884579ad7855ef19093674332a885a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-20 16:10:00 +0000

    feat: enhance user profile display with default values for telephone, location, and campus

--- UI/userProfile.js
+++ UI/userProfile.js
@@ -7,18 +7,22 @@ export async function createUserProfile() {
   const { firstName, lastName, tel, addressCity, email } =
     userProfileData[0].attrs;
 
+  const telContent = tel ?? "No telephone provided";
+  const addressContent = addressCity ?? "No location provided";
+  const campusContent = userProfileData[0].campus ?? "Unknown Campus";
+
   const fullName = createElement(
     `<strong>Full Name</strong>: ${firstName} ${lastName}`,
     "p"
   );
-  const telEl = createElement(`<strong>Tel</strong>: ${tel}`, "p");
+  const telEl = createElement(`<strong>Tel</strong>: ${telContent}`, "p");
   const emailEl = createElement(`<strong>Email</strong>: ${email}`, "p");
   const locationEl = createElement(
-    `<strong>Location</strong>: ${addressCity}`,
+    `<strong>Location</strong>: ${addressContent}`,
     "p"
   );
   const campusEl = createElement(
-    `<strong>Campus</strong>: ${userProfileData[0].campus}`,
+    `<strong>Campus</strong>: ${campusContent}`,
     "p"
   );
   personalInfo.append(fullName, emailEl, telEl, locationEl, campusEl);
