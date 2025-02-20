fef44ae0f6e2e5183393d815479221361d0c5b59
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-20 13:14:14 +0000

    feat: refactor transaction table functions to use async data retrieval for improved performance

--- UI/transactionTables.js
+++ UI/transactionTables.js
@@ -1,25 +1,33 @@
-import { transData } from "../data/data.js";
+import {
+  getGoTransactionData,
+  getJSTransactionData,
+  getXPsTransactionData,
+} from "../data/data.js";
 import { transTable } from "../queries/domQueries.js";
 import { createTable } from "./helperFunctions.js";
 
-export function javaScriptTransactionTable() {
+export async function javaScriptTransactionTable() {
   transTable.innerHTML = "";
   createTable(
-    transData.javaScriptTransactionData,
+    await getJSTransactionData(),
     "Your Transaction in JavaScript",
     transTable
   );
 }
 
-export function xpTransactionTable() {
-  transTable.innerHTML = "";
-  createTable(transData.xpTransactionData, "Your XPs transaction", transTable);
-}
-
-export function golangTransactionTable() {
+export async function xpTransactionTable() {
   transTable.innerHTML = "";
   createTable(
-    transData.golangTransactionData,
+    await getXPsTransactionData(),
+    "Your XPs transaction",
+    transTable
+  );
+}
+
+export async function golangTransactionTable() {
+  transTable.innerHTML = "";
+  createTable(
+    await getGoTransactionData(),
     "Your transactions in Golang",
     transTable
   );
